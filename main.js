cab8c75d2353090f82b095022dece9972a5c4871
Acrylix <1619025+acrylix@users.noreply.github.com>
2018-06-09 19:52:07 -0400

    added basic chain structure

--- main.js
+++ main.js
@@ -13,3 +13,30 @@ class Block{
     return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
   }
 }
+
+class Blockchain{
+  constructor(){
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock(){
+    return new Block(0, "01/01/2018", "Genesis Block", "0");
+  }
+
+  getLatestBlock(){
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock){
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock); //real blockchain will have more checks here
+  }
+}
+
+let AcrylixChain = new Blockchain();
+
+AcrylixChain.addBlock(new Block(1, "09/06/2018", {amount:999}));
+AcrylixChain.addBlock(new Block(2, "10/06/2018", {amount:1}));
+
+console.log(JSON.stringify(AcrylixChain, null, 4));
