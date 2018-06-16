25ca08604866294bb4530eb81c56de25f50f0d4f
Acrylix <1619025+acrylix@users.noreply.github.com>
2018-06-16 01:44:55 -0400

    updated tests

--- test/test.js
+++ test/test.js
@@ -12,4 +12,29 @@ describe('Blockchain', function() {
       assert(Chain.chain[0].transactions == 'Genesis Block');
     });
   });
+  describe('Mining', function(){
+    it('should mine pending tx', function(){
+      let Chain = new Blockchain();
+
+      Chain.createTransaction(new Transaction('address1', 'address2', 100));
+      Chain.createTransaction(new Transaction('address2', 'address1', 50));
+
+      Chain.minePendingTx('minerAddress1');
+
+      assert(Chain.pendingTx.length == 1);
+    });
+  });
+  describe('Block tampering', function(){
+    it('should transaction tampering return invalid chain', function(){
+      let Chain = new Blockchain();
+
+      Chain.createTransaction(new Transaction('address1', 'address2', 100));
+      Chain.createTransaction(new Transaction('address2', 'address1', 50));
+
+      Chain.minePendingTx('minerAddress1');
+
+      Chain.chain[1].transactions = {amount:999};
+      assert(!Chain.isChainValid());
+    });
+  });
 });
